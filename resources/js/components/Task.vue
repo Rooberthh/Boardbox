0ba81c0a42db9bc118d0e646c19c3e2994b4bc00
rooberthh <roberth.evaldsson@hotmail.com>
2019-04-26 08:13:31 +0200

    ui for task deletion

--- resources/js/components/Task.vue
+++ resources/js/components/Task.vue
@@ -4,7 +4,9 @@
             <div class="flex items-center">
                 <input type="text" class="text-default w-full" name="body" v-model="form.body">
 
+
                 <input type="checkbox" name="completed" v-model="form.completed" @change="update()">
+                <i class="fas fa-trash" @click="remove()"></i>
             </div>
     </div>
 </template>
@@ -33,7 +35,26 @@
                     .then(response => {
                         console.log(response.data);
                     });
+            },
+            remove() {
+                axios.delete(this.endpoint, this.id)
+                    .catch(error => {
+                        console.log(error);
+                    })
+                    .then(response => {
+                        console.log(response.data);
+                    });
+
+                this.$emit('deleted');
             }
         }
     }
 </script>
+
+<style scoped>
+    .fa-trash {
+        @apply .ml-3;
+        cursor: pointer;
+        color: #444;
+    }
+</style>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
     <!-- Styles -->
